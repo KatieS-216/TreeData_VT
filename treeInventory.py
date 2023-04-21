bf7a3cbf79b66b0a40999d9c71b2901c2fdd6d38
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-04-21 15:35:10 -0400

    Update treeInventory.py
    Removed vega_datasets

--- treeInventory.py
+++ treeInventory.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import altair as alt
 import streamlit as st
-from vega_datasets import data
 
 data = pd.read_csv('https://raw.githubusercontent.com/KatieS-216/TreeData_VT/main/Municipal_Tree_Inventory.csv')
 
@@ -12,9 +11,6 @@ col1 = st.columns(1)
 
 with col1:
   # CHART 1 - Point Map of Vermont
-
-  # Read in polygons from topojson
-  states = alt.topo_feature(data.us_10m.url, feature='states')
 
   # Vermont map
   states = alt.Chart(states_data).mark_geoshape(
