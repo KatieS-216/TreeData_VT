756d7d383dfbcb3b6cf6cb0724ed3e65ad3e8218
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-04-27 15:56:10 -0400

    Update treeInventory.py

--- treeInventory.py
+++ treeInventory.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import altair as alt
 import streamlit as st
+from pyproj import Transformer
 
 data = pd.read_csv('https://raw.githubusercontent.com/KatieS-216/TreeData_VT/main/Municipal_Tree_Inventory.csv', low_memory=False)
 
@@ -10,10 +11,18 @@ st.set_page_config(page_title="Vermont Tree Inventory", layout="wide", initial_s
 # mapCol = st.columns(1)
 # col2, col3= st.columns(2)
 
-d = {'lat': data['X'], 'lon': data['Y']}
+transf = Transformer.from_crs( "epsg:3684","epsg:4326",always_xy=False)
+
+x1, y1 = transf.transform(data['X'], data['Y'])
+
+#print(x1, y1)
+
+d = {'lat': x1, 'lon': y1}
 trees = pd.DataFrame(data=d)
 
-# with mapCol:
-#    st.map(trees)
+#trees.dropna(axis=1, inplace=True)
 
+# with mapCol:
 st.map(trees)
+
+#data['Y'].sample(10)
