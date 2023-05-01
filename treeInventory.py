59a2ae68dc8bde22b4fb717c393414bf7311deaa
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-04-30 23:10:50 -0400

    Update treeInventory.py

--- treeInventory.py
+++ treeInventory.py
@@ -17,9 +17,15 @@ x1, y1 = transf.transform(data['X'], data['Y'])
 
 #d = {'lat': x1, 'lon': y1}
 
-lat = x1
+"""lat = x1
 lon = y1
 
 trees = pd.DataFrame(columns=['lat','lon'])
 
-st.map(trees)
+st.map(trees)"""
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
