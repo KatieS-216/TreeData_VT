b0160e458796dcb94a89564923396973f57d345a
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-04-30 23:51:55 -0400

    Update treeInventory.py

--- treeInventory.py
+++ treeInventory.py
@@ -1,32 +1,43 @@
 import pandas as pd
 import altair as alt
-import streamlit as st
-import numpy as np
 from pyproj import Transformer
+import streamlit as st
+from vega_datasets import data
 
-data = pd.read_csv('https://raw.githubusercontent.com/KatieS-216/TreeData_VT/main/Municipal_Tree_Inventory.csv', low_memory=False)
-
-
-st.set_page_config(page_title="Vermont Tree Inventory", layout="wide", initial_sidebar_state="collapsed")
+treedata = pd.read_csv('https://raw.githubusercontent.com/KatieS-216/TreeData_VT/main/Municipal_Tree_Inventory.csv', low_memory=False)
 
 # mapCol = st.columns(1)
 # col2, col3= st.columns(2)
 
 transf = Transformer.from_crs( "epsg:3684","epsg:4326",always_xy=False)
 
-x1, y1 = transf.transform(data['X'], data['Y'])
+x1, y1 = transf.transform(treedata['X'], treedata['Y'])
 
 d = {'lat': x1, 'lon': y1}
 
-lat = x1
-lon = y1
+alt.data_transformers.disable_max_rows()
 
-trees = pd.DataFrame(d, columns=['lat','lon'])
+# Read trees in points
+trees = pd.DataFrame(d)
 
-st.map(trees)
+# Read in polygons from topojson
+states_data = alt.topo_feature(data.us_10m.url, feature='states')
 
-"""df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+# US states background
+states = alt.Chart(states_data).mark_geoshape(
+    fill='lightgray',
+    stroke='white'
+).project('albersUsa'
+).transform_filter((alt.datum.id == '22'))
 
-st.map(df)"""
+# tree positions on background
+points = alt.Chart(trees).mark_circle(
+    size=10,
+    color='steelblue'
+).encode(
+    longitude='lat:Q',
+    latitude='lon:Q',
+    #tooltip
+)
+
+states + points
