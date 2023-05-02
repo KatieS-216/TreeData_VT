fe5e71b47eebd72e541788058c8643b8ac0e7797
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-05-01 20:49:14 -0400

    Update treeInventory.py

--- treeInventory.py
+++ treeInventory.py
@@ -30,13 +30,6 @@ treemap = pd.DataFrame(
 
 st.map(treemap)
 
-# HISTOGRAM - Count of Tree Conditions
-hist_condition = alt.Chart(treedata, title="Count of Tree Conditions").mark_bar().encode(
-     x='ConditionID',
-     y='count()')
-    
-st.altair_chart(hist_condition)
-
 # BAR - GOOD Trees by Town
 bar_good = alt.Chart(treedata, title="Number of Good Trees by Town").mark_bar().encode(
       x='TOWN',
@@ -46,3 +39,10 @@ bar_good = alt.Chart(treedata, title="Number of Good Trees by Town").mark_bar().
 )
 
 st.altair_chart(bar_good)
+
+# HISTOGRAM - Count of Tree Conditions
+hist_condition = alt.Chart(treedata, title="Count of Tree Conditions").mark_bar().encode(
+     x='ConditionID',
+     y='count()')
+    
+st.altair_chart(hist_condition)
