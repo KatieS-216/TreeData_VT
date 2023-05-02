510836d6e866c08f1a05b2156ff5ce6cdfc2b537
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-05-01 21:55:51 -0400

    Update treeInventory.py

--- treeInventory.py
+++ treeInventory.py
@@ -71,14 +71,14 @@ st.altair_chart(hist_condition)
 
 #LAYERED HISTOGRAM - Average Tree Conditon by Town
 layereHist = alt.Chart(treedata).transform_fold(
-    'ConditionID',
-    as_=['Condition', 'Measurement']
+    'TOWN',
+    as_=['ConditionID', 'TOWN']
 ).mark_bar(
     opacity=0.3,
     binSpacing=0
 ).encode(
     alt.X('TOWN:Q').bin(maxbins=100),
-    alt.Y('count()').stack(None),
+    alt.Y('count(ConditionID)').stack(None),
     alt.Color('ConditionID:N')
 )
 
