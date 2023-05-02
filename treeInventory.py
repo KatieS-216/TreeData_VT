fa374900673e917cb7d8a2440958df28a1cb449e
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-05-01 20:07:24 -0400

    Update treeInventory.py
    Added title

--- treeInventory.py
+++ treeInventory.py
@@ -3,25 +3,31 @@ import altair as alt
 from pyproj import Transformer
 import streamlit as st
 import numpy as np
-# from vega_datasets import data
+import matplotlib.pyplot as plt
 
+# IMPORT TREE DATA
 treedata = pd.read_csv('https://raw.githubusercontent.com/KatieS-216/TreeData_VT/main/Municipal_Tree_Inventory.csv', low_memory=False)
-
-# mapCol = st.columns(1)
-#col2, col3= st.columns(2)
-
-transf = Transformer.from_crs( "epsg:3684","epsg:4326",always_xy=False)
 
 x1, y1 = transf.transform(treedata['X'], treedata['Y'])
 
 d = {'lat': x1, 'lon': y1}
 
-# alt.data_transformers.disable_max_rows()
+alt.data_transformers.disable_max_rows()
 
+# CONVERT COORDINATES
+transf = Transformer.from_crs( "epsg:3684","epsg:4326",always_xy=False)
+
+# USE CUSTOM MAP TOKEN
 token = 'pk.eyJ1Ijoia2F0aWUtcy0yMTYiLCJhIjoiY2xoNWhrdjdwMDE1OTNkcDUwMDZ0b3hqYiJ9.kov77lplpJ-rqzlQeExusw'
 
+# TITLE
+st.title('Vermont Tree Inventory')
+
+# MAP - Tree Locations
 treemap = pd.DataFrame(
     d,
     columns=['lat', 'lon']).dropna()
 
 st.map(treemap)
+
+
