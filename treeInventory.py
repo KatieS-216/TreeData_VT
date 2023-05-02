87fdbca53ea5db1a22d2af708cf51b34b6f55a91
KatieS-216 <70543134+KatieS-216@users.noreply.github.com>
2023-05-01 21:14:58 -0400

    Update treeInventory.py

--- treeInventory.py
+++ treeInventory.py
@@ -36,12 +36,23 @@ st.divider()
 # BAR - GOOD Trees by Town
 bar_good = alt.Chart(treedata, title="Number of Good Trees by Town").mark_bar().encode(
     x='TOWN',
-    y= alt.Y('count(ConditionID)', title="Count of Good Trees")
+    y= alt.Y('count(ConditionID)', title="Number of Trees")
   ).transform_filter(
     alt.FieldEqualPredicate(field='ConditionID', equal='GOOD')
 )
 
 st.altair_chart(bar_good)
+
+#DIVIDER
+st.divider()
+
+# BAR - Number of Trees per Town
+bar_number = alt.Chart(treedata, title="Number of Trees per Town").mark_bar().encode(
+    x='TOWN',
+    y= alt.Y('count(OBJECTID)', title="Count of Good Trees")
+  )
+
+st.altair_chart(bar_number)
 
 #DIVIDER
 st.divider()
